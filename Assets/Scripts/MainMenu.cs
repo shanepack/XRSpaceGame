d2ad89663e3246dbfee60a877f818239fcc4a9eb
Tyler <90929371+tyler-cog@users.noreply.github.com>
2024-11-17 02:31:34 -0600

    Button / Grabbing Tests
    Possible to click button and grab objects, to be used later.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitioner : MonoBehaviour
+{
+    public void PlayBtn() {
+        Debug.Log("PlayBtn clicked");
+        SceneManager.LoadScene("SampleScene");    }
+    public void AboutBtn() {
+        Debug.Log("AboutBtn clicked");
+        
+    }
+    public void OptionsBtn() {
+        Debug.Log("OptionsBtn clicked");
+    }
+    public void QuitBtn() {
+        Debug.Log("QuitBtn clicked");
+        Application.Quit();
+    }
+}
